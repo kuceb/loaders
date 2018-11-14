ea081ecc926c1c9646ff05aa17397b406073ba1d
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2018-11-14 01:30:03 -0500

    cleanup, polish, add id + form

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,83 @@
 <template>
   <v-app>
-    <v-toolbar app height="40px" flat>
-      <v-toolbar-title class="headline" >
+    <v-toolbar
+      app
+      height="40px"
+      flat
+    >
+      <v-toolbar-title class="headline">
         <span>Psycode</span>
         <span class="font-weight-light"> Experiment</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
+      <v-spacer />
+      id:&nbsp;<span class="font-weight-bold"> {{ id }}</span>
     </v-toolbar>
 
     <v-content>
-      <v-container align-center justify-center="" fill-height>
-      <v-slide-x-transition mode="out-in">
-      <div v-if="menu" key="menu">
-      Welcome to the Psycode experiment. When you're ready, press continue
-      <v-btn  @click="menu = false; info = true">Continue</v-btn>
-      </div>
-      <div v-else-if="info">
-      <v-text-field v-model="name" required label="name"/>
-      <v-btn  @click="if (name) info = false">Start</v-btn>
-      </div >
-      <Experiment :name="name" v-else key="app"/>
-      </v-slide-x-transition>
+      <v-container
+        fill-height=""
+        align-center
+      >
+        <v-layout>
+          <v-slide-y-transition mode="out-in">
+            <v-flex
+              v-if="menu"
+              key="menu"
+              text-xs-center
+              column=""
+            >
+              <v-flex
+                title
+                font-weight-light
+              >
+                <p>Welcome to the Psycode experiment. Before we being, here's a survey.</p><p>
+                  Your results will be anonymized.</p>
+              </v-flex>
+              <v-btn
+                target="blank"
+                :href="`https://docs.google.com/forms/d/e/1FAIpQLSc9PCjwowMc5Anpc_f2_LVIdC7ctjpAogN0qOveaqP4_gtV_w/viewform?entry.795072666=${id}`"
+                @click="menu = false; info = true"
+              >Survey</v-btn>
+            </v-flex>
+            <v-flex
+              v-else-if="info"
+              text-xs-center
+            >
+              <v-flex>
+                <v-btn
+                  @click="start()"
+                >Start</v-btn>
+              </v-flex>
+            </v-flex>
+            <Experiment
+              v-else
+              :id="id"
+              key="app"
+            />
+          </v-slide-y-transition>
+        </v-layout>
       </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Experiment from './components/Experiment'
+import Experiment from './components/Experiment.vue'
 
 export default {
   name: 'App',
   components: {
-    Experiment
+    Experiment,
   },
-  data: ()=>({
+  data: () => ({
     menu: true,
     info: false,
-  })
+    id: Math.round(Math.random() * 10000000).toString(16),
+  }),
+  methods: {
+    start() {
+      this.info = false
+    },
+  },
 }
 </script>
--- src/components/Experiment.vue
+++ src/components/Experiment.vue
@@ -1,168 +1,210 @@
 <template>
-  <v-tab-transition mode="out-in">
-    <div v-if="waiting">
-      click to continue to the next loader.<br>
-      <v-btn @click="startLoading()">Continue</v-btn>
-    </div>
-    <div v-else-if="loading">
-      <div v-if="currentResult.indicator.complexity==='none'">
-        loading...
-      </div> 
-      <div v-else-if="currentResult.indicator.complexity==='simple'">
-        <div class="ball">
-          <div></div>
-        </div>
-      </div> 
-      <div v-else-if="currentResult.indicator.complexity==='moderate'">
-        <div class='ball'>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </div> 
-      <div v-else-if="currentResult.indicator.complexity==='complex'">
-        <img src="../assets/pig.gif"/>
+  <v-layout
+    text-xs-center
+    justify-center
+    align-center
+  >
+    <v-slide-y-transition mode="out-in">
+      <div
+        v-if="waiting"
+        key="waiting"
+      >
+        click to continue to the next loader.<br>
+        <v-btn @click="startLoading()">Continue</v-btn>
       </div>
-      <div align-center> 
-      <v-subheader v-if="currentResult.indicator.determinant">
-          {{Math.round(currentPercentage)}}%
-      </v-subheader>
-    </div>
-    </div>
-    <div v-else-if="afterLoading">
-      <div v-if="!this.pressTime"> {{pressing ?'release when ready': 'Hold spacebar'}}</div>
-      <v-btn v-else @click="startWaiting()">Submit</v-btn>
-    </div>
-    <div v-else-if="done">
-      Done! thank you, have a nice day.
-    </div>
+      <div
+        v-else-if="loading"
+        class="no-anim"
+      >
+        <div v-if="currentResult.indicator.complexity==='none'">
+          loading...
+        </div>
+        <div v-else-if="currentResult.indicator.complexity==='simple'">
+          <div class="ball">
+            <div />
+          </div>
+        </div>
+        <div v-else-if="currentResult.indicator.complexity==='moderate'">
+          <div class="ball">
+            <div />
+            <div />
+            <div />
+          </div>
+        </div>
+        <div v-else-if="currentResult.indicator.complexity==='complex'">
+          <img src="../assets/pig.gif">
+        </div>
+
+        <v-flex
+          v-if="currentResult.indicator.determinant"
+          class="headline"
+          mt-4
+        >
+          {{ Math.round(currentPercentage) }}%
+        </v-flex>
+
+      </div>
+      <div
+        v-else-if="afterLoading"
+        key="afterLoading"
+      >
+        <v-scale-transition mode="out-in">
+          <div
+            v-if="!pressTime"
+            key="sd"
+          >
+            <!-- <v-fade-transition mode="out-in"> -->
+            <span
+              v-if="!pressing"
+              key="1"
+            >Hold the spacebar</span>
+            <span
+              v-else
+              key="2"
+            >Release when ready</span>
+            <!-- </v-fade-transition> -->
+
+          </div>
+          <div
+            v-else
+            key="button"
+          >
+            <v-btn
+              dark
+              @click="pressTime=0;pressing=0"
+            ><v-icon left>undo</v-icon>redo</v-btn>
+            <v-btn
+              @click="startWaiting()"
+            >Submit</v-btn>
+          </div>
+        </v-scale-transition>
+      </div>
+      <div v-else-if="done">
+        Done!<br> You may close this window.
+      </div>
 
 
-  </v-tab-transition>
-</template>
+    </v-slide-y-transition>
+</v-layout></template>
 
 <script>
-  import * as _ from "lodash";
-  const complexityTypes = ['none', 'simple', 'moderate', 'complex']
-  const MAX_TIME = 5000
+import * as _ from 'lodash'
 
-  const indicators = _.shuffle(_.flatten(
-    _.map(complexityTypes, (v) => [
-    {complexity:v, determinant:true},
-    {complexity:v, determinant:false}
-    ])
-  ))
-  console.log(indicators)
+const complexityTypes = ['none', 'simple', 'moderate', 'complex']
+const MIN_TIME = 2000
+const MAX_TIME = 4000
 
-  let start = 0;
+const indicators = _.shuffle(_.flatten(
+  _.map(complexityTypes, v => [
+    { complexity: v, determinant: false },
+    { complexity: v, determinant: true },
+  ]),
+))
+
+let start = 0
 
 
-  export default {
-    props: {
-      name
+export default {
+  props: {
+    id: {
+      type: String,
+      default: 'anon',
     },
-    data: () => ({
-      results: [],
-      loading: false,
-      afterLoading:false,
-      waiting: true,
-      currentResult: {
-        indicator: {
-          complexity: 'simple',
-          determinant: true,
-        }
-        //null
+  },
+  data: () => ({
+    waiting: true,
+    loading: false,
+    afterLoading: false,
+    done: false,
+    results: [],
+    currentResult: {
+      indicator: {
+        complexity: 'moderate',
+        determinant: true,
       },
-      currentPercentage: 0,
-      pressTime: 0,
-      pressing: false,
-      done: false
-    }),
+      // null
+    },
+    currentPercentage: 0,
+    pressTime: 0,
+    pressing: false,
+  }),
 
-    mounted() {
-      console.log(this.name)
-      setInterval(this.countUp,100)//Call increaseMyVar every 30ms
-      setInterval(()=>console.log(this.pressTime), 1000)
-      window.addEventListener('keydown', this.processKeyDown)
+  mounted() {
+    window.addEventListener('keydown', this.processKeyDown)
+    window.addEventListener('keyup', this.processKeyUp)
+  },
 
-      window.addEventListener('keyup', this.processKeyUp)
+  methods: {
+    countUp() {
+      this.currentPercentage = Math.min(this.currentPercentage + 5000 / (this.currentResult.real || 1), 99)
+    },
+    startLoading() {
+      this.waiting = false
+      this.loading = true
+
+      this.currentResult = {
+        real: Math.round(MIN_TIME + Math.random() * (MAX_TIME - MIN_TIME)),
+        indicator: indicators.shift(),
+        guess: null,
+      }
+      this.currentPercentage = 0
+      const timer = setInterval(this.countUp, 50)// Call increaseMyVar every 30ms
+
+
+      setTimeout(() => {
+        clearInterval(timer)
+        this.startAfterLoading()
+      }, this.currentResult.real)
     },
 
-    methods: {
-      countUp() {
-        console.log('count up')
-          this.currentPercentage = (this.currentPercentage + 10000/(this.currentResult.real||1));
-          console.log(this.currentPercentage)
-      },
-      startLoading() {
-        this.waiting = false
-        this.loading = true
-        
-        this.currentResult = {
-          real: Math.round(3000+Math.random()*MAX_TIME),
-          indicator: indicators.shift(),
-          guess: null
-        }
-        this.currentPercentage = 0
-
-        
-
-        setTimeout(()=>{
-          this.startAfterLoading()
-        }, this.currentResult.real)
-      },
-
-      startAfterLoading() {
-        this.pressing = false
-        this.pressTime = 0
-        this.loading =false
-        this.afterLoading = true
-      },
-
-      startWaiting() {
-        this.currentResult.guess = this.pressTime
-        this.results.push(_.cloneDeep(this.currentResult))
-        this.afterLoading = false
-        this.waiting = true
-        console.log(JSON.stringify({
-            user: name, info: this.results
-              }))
-        console.log('indeixctor length', indicators.length)
-        if (!indicators.length) {
-          this.endExperiment()
-        }
-      },
-
-      async endExperiment() {
-        this.waiting = false
-        this.done = true
-        await fetch('https://psycode-375bf.firebaseio.com/data.json', {
-          method: 'POST',
-           headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+    startAfterLoading() {
+      this.pressing = false
+      this.pressTime = 0
+      this.loading = false
+      this.afterLoading = true
     },
-          body: JSON.stringify({
-            user: this.name, info: this.results
-              })
+
+    startWaiting() {
+      this.currentResult.guess = this.pressTime
+      this.results.push(_.cloneDeep(this.currentResult))
+      this.afterLoading = false
+      this.waiting = true
+
+      if (!indicators.length) {
+        this.endExperiment()
+      }
+    },
+
+    async endExperiment() {
+      this.waiting = false
+      this.done = true
+      await fetch('https://psycode-375bf.firebaseio.com/data.json', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: this.id, info: this.results,
+        }),
       })
-      },
+    },
     processKeyDown() {
       if (!start) {
-          this.pressing = true
-          start = (new Date()).getTime();
+        this.pressing = true
+        start = (new Date()).getTime()
       }
     },
 
     processKeyUp() {
       this.pressing = false
-    var delta = (new Date()).getTime() - start;
-    this.pressTime = delta
-    start = 0;
+      const delta = (new Date()).getTime() - start
+      this.pressTime = delta
+      start = 0
+    },
+
+  },
 }
-      
-    }
-  }
 </script>
 
 <style>
@@ -191,5 +233,9 @@
   50% {
     transform: translateY(-80px);
   }
+}
+
+.no-anim {
+  transition: 0s
 }
 </style>
